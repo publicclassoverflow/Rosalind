ce815f341965e41ccfe06bf90d3e34694d6db216
meng <mz.zhoumeng@gmail.com>
2017-06-01 01:50:58 -0700

    Started. Modified the algorithm of Counting DNA Nucleotides.

--- CountNucleotides/CountNucleotides.py
+++ CountNucleotides/CountNucleotides.py
@@ -1,19 +1,26 @@
 # http://rosalind.info/problems/dna/
-# A string is simply an ordered collection of symbols selected from some alphabet and formed into a word; the length of
+# A string is simply an ordered collection of symbols selected from some
+# alphabet and formed into a word; the length of
 # a string is the number of symbols that it contains.
 
-# An example of a length 21 DNA string (whose alphabet contains the symbols 'A', 'C', 'G', and 'T')
+# An example of a length 21 DNA string (whose alphabet contains the symbols
+# 'A', 'C', 'G', and 'T')
 # is "ATGCTTCAGAAAGGTCTTACG."
 
 # Given: A DNA string ss of length at most 1000 nt.
-# Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G',
+# Return: Four integers (separated by spaces) counting the respective number of
+# times that the symbols 'A', 'C', 'G',
 # and 'T' occur in ss.
 
 
 def count_nucleotides(dna_sequence):
     result = {}
-    for base in dna_sequence:
-        result[base] = dna_sequence.count(base)
+    # for base in dna_sequence:
+    #     result[base] = dna_sequence.count(base)
+    # The method above has a complexity of O(4N)
+    # The one below is O(N)
+    for i in range(len(dna_sequence)):
+        result[dna_sequence[i]] = result.get(dna_sequence[i], 0) + 1
     return result
 
 
